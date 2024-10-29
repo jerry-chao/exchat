b0ffb764883d7c963fc0a20881bd39ead1722eee
jerry.zhang <jerry.zhang>
2024-10-29 19:34:04 +0800

    format without pb files

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,4 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "config/*.exs"],
-  subdirectories: ["apps/*"]
+  inputs: Enum.flat_map(["mix.exs", "config/*.exs", "apps/*/lib/**/*.{ex,exs}", "apps/*/test/*.exs"], &Path.wildcard(&1, match_dot: true)) -- Path.wildcard("apps/*/lib/**/*.pb.ex", match_dot: true)
 ]
