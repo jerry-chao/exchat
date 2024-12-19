99a2a743c3af5253a409792e2d6ff5483b5aa298
张超 <462283159@qq.com>
2024-12-19 10:58:12 +0800

    upgrade live view from 0.x to 1.x

--- apps/exchat/mix.exs
+++ apps/exchat/mix.exs
@@ -52,7 +52,7 @@ defmodule Exchat.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run apps/exchat/priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
